bea1ca72f1e307180e1819c14041662cd50b6a77
Mr. Burger <bburger@d155.org>
2020-09-11 18:24:29 +0000

    Added examples

--- Main.java
+++ Main.java
@@ -1,10 +1,13 @@
 class Main {
   public static void main(String[] args) {
+   
     System.out.println("Hello world!");
     System.out.println("\"Hello world!\"");
-    System.out.println("Hello \nworld!");
+    System.out.println("Hello\nworld!");
     System.out.println("Hello\tworld!");
     System.out.println("Hi\t\tthere!");
+    System.out.println("row\t\tthree!");
     System.out.println("\\Hello world!\\\\\\");
+
   }
 }
