d4768ac89ddc28b7238033ae9d4a021bcbf4cc4b
Mr. Burger <bburger@d155.org>
2020-09-11 17:29:47 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    System.out.println("\"Hello world!\"");
+    System.out.println("Hello \nworld!");
+    System.out.println("Hello\tworld!");
+    System.out.println("Hi\t\tthere!");
+    System.out.println("\\Hello world!\\\\\\");
+  }
+}
